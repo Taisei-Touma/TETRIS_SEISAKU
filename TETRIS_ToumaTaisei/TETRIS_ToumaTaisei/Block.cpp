bc8ab4dd38b84c62b46797413b56528b9a95f1dd
Taisei-Touma <138739388+Taisei-Touma@users.noreply.github.com>
2023-10-03 15:00:34 +0900

    １０/３(月曜日)　1回目
    Block_cppの１６行まで打ち込んだ

---
+++ TETRIS_ToumaTaisei/TETRIS_ToumaTaisei/Block.cpp
@@ -0,0 +1,19 @@
+#include "Block.h"
+#include "DxLib.h"
+#include "InputControl.h"
+
+/******************************
+* マクロ定義
+******************************/
+#define FIELD_HEIGHT			(21)
+#define FIElD_WIDTH				(12)
+#define BLOCK_TROUT_SIZE		(4)
+#define BLOCK_SIZE				(36)
+#define BLOCK_TYPE_MAX			(7)
+#define BLOCK_NEXT_POS_X		(700)
+#define BLOCK_NEXT_POS_Y		(500)
+#define BLOCK_STOCK_POS_X　　　(500)
+#define BLOCK_STOCK_POS_Y
+
+
+
