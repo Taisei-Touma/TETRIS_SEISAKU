f464b98e816e064b810bafeefad4856e5007d896
Taisei-Touma <138739388+Taisei-Touma@users.noreply.github.com>
2023-11-02 10:49:09 +0900

    11/2 3

--- TETRIS_ToumaTaisei/TETRIS_ToumaTaisei/TitleScene.cpp
+++ TETRIS_ToumaTaisei/TETRIS_ToumaTaisei/TitleScene.cpp
@@ -130,9 +130,9 @@ void TitleScene_Draw(void)
 {
 	SetFontSize(50);
 	DrawString(100, 100, "テトリス", GetColor(255, 255, 255));
-	DrawString(100, 100, "スタート", GetColor(255, 255, 255));
-	DrawString(100, 100, "ランキング", GetColor(255, 255, 255));
-	DrawString(100, 100, "エンド", GetColor(255, 255, 255));
+	DrawString(300, 300, "スタート", GetColor(255, 255, 255));
+	DrawString(300, 350, "ランキング", GetColor(255, 255, 255));
+	DrawString(300, 400, "エンド", GetColor(255, 255, 255));
 	SetFontSize(20);
 
 	DrawCircle(275, 325 + (cursor_number * 50), 15, GetColor(255, 0, 0));
